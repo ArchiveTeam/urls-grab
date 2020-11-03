05bcd309725e873026c2e054fe725565bec33922
arkiver <arkiver@protonmail.com>
2020-11-03 02:38:40 +0100

    Version 20201103.02. Experimental and temporary timeout for Wget-AT command.

--- pipeline.py
+++ pipeline.py
@@ -49,7 +49,7 @@ if not WGET_AT:
 #
 # Update this each time you make a non-cosmetic change.
 # It will be added to the WARC files and reported to the tracker.
-VERSION = '20201103.01'
+VERSION = '20201103.02'
 USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
 TRACKER_ID = 'urls'
 TRACKER_HOST = 'trackerproxy.archiveteam.org'
@@ -178,6 +178,7 @@ def stats_id_function(item):
 class WgetArgs(object):
     def realize(self, item):
         wget_args = [
+            'timeout', '360',
             WGET_AT,
             '-U', USER_AGENT,
             '-nv',
