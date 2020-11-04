104776d6a9c25e43bf5e68e8adaec78d2ed090ef
arkiver <arkiver@protonmail.com>
2020-11-04 23:43:47 +0100

    Version 20201104.02. Queue URLs without parameters and with certain parameters removed.

--- urls.lua
+++ urls.lua
@@ -1,4 +1,5 @@
 local urlparse = require("socket.url")
+local http = require("socket.http")
 
 local item_dir = os.getenv('item_dir')
 local item_name = os.getenv("item_name")
@@ -6,6 +7,13 @@ local warc_file_base = os.getenv('warc_file_base')
 
 local url_count = 0
 local downloaded = {}
+local abortgrab = false
+
+if urlparse == nil or http == nil then
+  io.stdout:write("socket not corrently installed.\n")
+  io.stdout:flush()
+  abortgrab = true
+end
 
 local urls = {}
 for url in string.gmatch(item_name, "([^\n]+)") do
@@ -14,6 +22,7 @@ end
 
 local current_url = nil
 local bad_urls = {}
+local queued_urls = {}
 
 bad_code = function(status_code)
   return status_code == 0
@@ -26,6 +35,36 @@ bad_code = function(status_code)
     or status_code == 429
     or status_code == 451
     or status_code >= 500
+end
+
+remove_param = function(url, param)
+  url = string.gsub(url, "([%?&])" .. param .. "=[^%?&]*[%?&]?", "%1")
+  return string.match(url, "^(.-)[%?&]?$")
+end
+
+queue_new_urls = function(url)
+  local newurl = remove_param(url, "utm_source")
+  newurl = remove_param(newurl, "utm_medium")
+  newurl = remove_param(newurl, "utm_campaign")
+  newurl = remove_param(newurl, "utm_term")
+  newurl = remove_param(newurl, "utm_content")
+  newurl = remove_param(newurl, "utm_adgroup")
+  newurl = remove_param(newurl, "ref")
+  newurl = remove_param(newurl, "refsrc")
+  newurl = remove_param(newurl, "referrer_id")
+  newurl = remove_param(newurl, "referrerid")
+  newurl = remove_param(newurl, "src")
+  newurl = remove_param(newurl, "i")
+  newurl = remove_param(newurl, "s")
+  newurl = remove_param(newurl, "ts")
+  newurl = remove_param(newurl, "feature")
+  if newurl ~= url then
+    queued_urls[newurl] = true
+  end
+  newurl = string.match(url, "^([^%?&]+)")
+  if newurl ~= url then
+    queued_urls[newurl] = true
+  end
 end
 
 wget.callbacks.write_to_warc = function(url, http_stat)
@@ -58,6 +97,10 @@ wget.callbacks.httploop_result = function(url, err, http_stat)
     downloaded[url["url"]] = true
   end
 
+  if status_code >= 200 and status_code < 300 then
+    queue_new_urls(url["url"])
+  end
+
   if bad_code(status_code) then
     io.stdout:write("Server returned " .. http_stat.statcode .. " (" .. err .. ").\n")
     io.stdout:flush()
@@ -79,10 +122,44 @@ wget.callbacks.httploop_result = function(url, err, http_stat)
 end
 
 wget.callbacks.finish = function(start_time, end_time, wall_time, numurls, total_downloaded_bytes, total_download_time)
+  local newurls = nil
+  for url, _ in pairs(queued_urls) do
+    if newurls == nil then
+      newurls = url
+    else
+      newurls = newurls .. "\0" .. url
+    end
+  end
+
+  if newurls ~= nil then
+    local tries = 0
+    while tries < 10 do
+      local body, code, headers, status = http.request(
+        "http://blackbird-amqp.meo.ws:23038/urlstest-m92hrwe0faimbhi/",
+        newurls
+      )
+      if code == 200 or code == 409 then
+        break
+      end
+      os.execute("sleep " .. math.floor(math.pow(2, tries)))
+      tries = tries + 1
+    end
+    if tries == 10 then
+      abortgrab = true
+    end
+  end
+
   local file = io.open(item_dir .. '/' .. warc_file_base .. '_bad-urls.txt', 'w')
   for url, _ in pairs(bad_urls) do
     file:write(url .. "\n")
   end
   file:close()
+end
+
+wget.callbacks.before_exit = function(exit_status, exit_status_string)
+  if abortgrab then
+    return wget.exits.IO_FAIL
+  end
+  return exit_status
 end
 
