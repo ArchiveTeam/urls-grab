1063f1195681dfeaf8fea0d7ab9d8bcda9cb6b05
arkiver <arkiver@protonmail.com>
2020-11-06 17:38:54 +0100

    Version 20201106.01. Increase Wget-AT process timeout to 600 seconds.

--- pipeline.py
+++ pipeline.py
@@ -50,7 +50,7 @@ if not WGET_AT:
 #
 # Update this each time you make a non-cosmetic change.
 # It will be added to the WARC files and reported to the tracker.
-VERSION = '20201104.04'
+VERSION = '20201106.01'
 USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.183 Safari/537.36'
 TRACKER_ID = 'urls'
 TRACKER_HOST = 'trackerproxy.archiveteam.org'
@@ -214,7 +214,7 @@ class ZstdDict(object):
 class WgetArgs(object):
     def realize(self, item):
         wget_args = [
-            'timeout', '360',
+            'timeout', '600',
             WGET_AT,
             '-U', USER_AGENT,
             '-nv',
