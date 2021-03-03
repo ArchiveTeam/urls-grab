7a9575b85a28482fb441dccef8d0e81605a82ec4
arkiver <arkiver@protonmail.com>
2021-03-03 15:57:17 +0100

    Version 20210303.01. Disable getting page requisites.

--- urls.lua
+++ urls.lua
@@ -152,6 +152,10 @@ wget.callbacks.download_child_p = function(urlpos, parent, depth, start_url_pars
     return true
   end
 
+  if true then
+    return false
+  end
+
   if not verdict then
     return false
   end
