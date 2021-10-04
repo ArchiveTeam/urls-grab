2bd944ded2afae575447dbcae16fc2e9d60b3258
arkiver <arkiver@protonmail.com>
2021-10-04 21:04:54 +0200

    Version 20211004.01. Do not check facebook.com while down at the moment.

--- pipeline.py
+++ pipeline.py
@@ -66,7 +66,7 @@ if not WGET_AT:
 #
 # Update this each time you make a non-cosmetic change.
 # It will be added to the WARC files and reported to the tracker.
-VERSION = '20211001.01'
+VERSION = '20211004.01'
 #USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.183 Safari/537.36'
 TRACKER_ID = 'urls'
 TRACKER_HOST = 'legacy-api.arpa.li'
@@ -92,7 +92,7 @@ class CheckIP(SimpleTask):
             ip_set = set()
 
             ip_set.add(socket.gethostbyname('twitter.com'))
-            ip_set.add(socket.gethostbyname('facebook.com'))
+            #ip_set.add(socket.gethostbyname('facebook.com'))
             ip_set.add(socket.gethostbyname('youtube.com'))
             ip_set.add(socket.gethostbyname('microsoft.com'))
             ip_set.add(socket.gethostbyname('icanhas.cheezburger.com'))
