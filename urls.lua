91be5757c57d437e01fdec0c137ac638e7dc2e8f
arkiver <arkiver@protonmail.com>
2022-03-05 16:08:47 +0100

    Version 20220305.02. Set max path repetitions to 2.

--- urls.lua
+++ urls.lua
@@ -246,7 +246,7 @@ queue_url = function(url, withcustom)
     end
   end
   if not duplicate_urls[url] and not queued_urls[url] then
-    if find_path_loop(url, 3) then
+    if find_path_loop(url, 2) then
       return false
     end
 --print('queuing',original, url)
@@ -320,7 +320,7 @@ wget.callbacks.download_child_p = function(urlpos, parent, depth, start_url_pars
     return true
   end
 
-  if find_path_loop(url, 3) then
+  if find_path_loop(url, 2) then
     return false
   end
 
