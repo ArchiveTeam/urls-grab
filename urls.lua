bcb0628cbf741b281263a2d2355dbef100e7f9ad
arkiver <arkiver@protonmail.com>
2022-06-16 19:54:43 +0200

    Version 20220616.03. Extract URLs from robots.txt.

--- urls.lua
+++ urls.lua
@@ -698,10 +698,14 @@ wget.callbacks.get_urls = function(file, url, is_css, iri)
     for line in string.gmatch(html, "(.-)\n") do
       local name, path = string.match(line, "([^:]+):%s*(.-)%s+$")
       if name and path then
+        -- the path should normally be absolute already
+        local newurl = urlparse.absolute(url, path)
         if string.lower(name) == "sitemap" then
-          -- the path should normally be absolute already
-          local newurl = urlparse.absolute(url, path)
           queue_monthly_url(newurl)
+        elseif string.lower(name) ~= "user-agent"
+          and not string.match(path, "%*")
+          and not string.match(path, "%$") then
+          queue_url(newurl)
         end
       end
     end
