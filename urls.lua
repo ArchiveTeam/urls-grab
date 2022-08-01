5787d993fc3beb64f45ec70f8d5121961e778b03
arkiver <arkiver@protonmail.com>
2022-08-01 18:30:59 +0200

    Version 20220802.01. Improve patterns to ignore for queuing special documents. Check new URL instead of parent URL,

--- urls.lua
+++ urls.lua
@@ -429,7 +429,8 @@ wget.callbacks.download_child_p = function(urlpos, parent, depth, start_url_pars
       or string.match(parenturl, "[^a-z0-9A-Z]" .. string.upper(extension) .. "$")
       or string.match(parenturl, "[^a-z0-9A-Z]" .. string.upper(extension) .. "[^a-z0-9A-Z]")
       -- get rid of loop on sites from chinese origin (also various non-.cn domains)
-      or string.match(parenturl, "^https?://[^/]+/.+%.[a-z]+%?/.+%." .. extension .. "$") then
+      or string.match(url, "^https?://[^/]+/%?/.+%." .. extension .. "$")
+      or string.match(url, "^https?://[^/]+/.+%.[a-z]+%?/.+%." .. extension .. "$") then
       return false
     end
     if string.match(url, "[^a-z0-9A-Z]" .. extension .. "$")
