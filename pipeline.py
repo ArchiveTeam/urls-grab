f77165553638f0d1698aa3242c84e014622f3206
arkiver <arkiver@protonmail.com>
2023-06-16 15:56:31 +0200

    Version 20230616.02. Run checks every 30 multi items.

--- pipeline.py
+++ pipeline.py
@@ -68,7 +68,7 @@ if not WGET_AT:
 #
 # Update this each time you make a non-cosmetic change.
 # It will be added to the WARC files and reported to the tracker.
-VERSION = '20230616.01'
+VERSION = '20230616.02'
 #USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.183 Safari/537.36'
 TRACKER_ID = 'urls'
 TRACKER_HOST = 'legacy-api.arpa.li'
@@ -160,7 +160,7 @@ class CheckIP(SimpleTask):
 
         # Check only occasionally
         if self._counter <= 0:
-            self._counter = 10
+            self._counter = 30
         else:
             self._counter -= 1
 
