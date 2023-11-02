d99c6ba6958e974361f39f416d600b70ce49f2eb
arkiver <arkiver@protonmail.com>
2023-11-02 22:48:06 +0100

    Version 20231102.01. Check for minimum version of Wget-AT instead of specific version.

--- pipeline.py
+++ pipeline.py
@@ -48,11 +48,23 @@ LOCK = threading.Lock()
 # 1. does not crash with --version, and
 # 2. prints the required version string
 
+class HigherVersion:
+    def __init__(self, expression, min_version):
+        self._expression = re.compile(expression)
+        self._min_version = min_version
+
+    def search(self, text):
+        for result in self._expression.findall(text):
+            if result >= self._min_version:
+                print('Found version {}.'.format(result))
+                return True
+
 WGET_AT = find_executable(
     'Wget+AT',
-    [
+    HigherVersion(
+        r'(GNU Wget 1\.[0-9]{2}\.[0-9]{1}-at\.[0-9]{8}\.[0-9]{2})[^0-9a-zA-Z\.-_]',
         'GNU Wget 1.21.3-at.20230623.01'
-    ],
+    ),
     [
         './wget-at',
         '/home/warrior/data/wget-at'
@@ -68,7 +80,7 @@ if not WGET_AT:
 #
 # Update this each time you make a non-cosmetic change.
 # It will be added to the WARC files and reported to the tracker.
-VERSION = '20231031.01'
+VERSION = '20231102.01'
 #USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.183 Safari/537.36'
 TRACKER_ID = 'urls'
 TRACKER_HOST = 'legacy-api.arpa.li'
