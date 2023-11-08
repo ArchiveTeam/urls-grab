c7c2718124f502d3ac0dc0c0a16f65c7345779a7
arkiver <arkiver@protonmail.com>
2023-11-08 22:19:57 +0100

    Version 20231108.05. Queue again from ads.txt and app-ads.txt. Do not queue URL to which is 301 redirected with if it a front page without trailing /.

--- urls.lua
+++ urls.lua
@@ -1337,14 +1337,14 @@ wget.callbacks.get_urls = function(file, url, is_css, iri)
           for line in string.gmatch(html, "(.-)\n") do
             if not string.match(line, "^#") then
               local site = string.match(line, "^([^,%s]+),")
-              --[[if site then
+              if site then
                 if string.match(site, "^https?://") then
                   queue_url(site)
                 else
                   queue_url("http://" .. site .. "/")
                   queue_url("https://" .. site .. "/")
                 end
-              end]]
+              end
             end
           end
         end
@@ -1610,7 +1610,10 @@ wget.callbacks.httploop_result = function(url, err, http_stat)
         string.match(url["url"], "^https?://www%.(.+)")
         or string.match(url["url"], "^https?://(.+)")
       )
-      or status_code == 301
+      or (
+        status_code == 301
+        and not string.match(newloc, "^https?://[^/]+$")
+      )
       or status_code == 303
       or status_code == 308 then
       queue_url(newloc)
