15375105fcfef3872ccc15f97c52f5a92ce85bdb
arkiver <arkiver@protonmail.com>
2024-04-11 18:15:49 +0200

    Version 20240411.04. Increase items to submit at once to 400 from 100.

--- urls.lua
+++ urls.lua
@@ -1781,7 +1781,7 @@ wget.callbacks.finish = function(start_time, end_time, wall_time, numurls, total
                 newurls = newurls .. "\0" .. url
               end
               count = count + 1
-              if count == 100 then
+              if count == 400 then
                 submit_backfeed(newurls, key, shard)
                 newurls = nil
                 count = 0
