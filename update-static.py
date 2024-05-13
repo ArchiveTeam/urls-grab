94765687d17ca254c511115b64e37d7dcf54b420
arkiver <arkiver@protonmail.com>
2024-05-13 12:07:21 +0200

    Version 20240513.01. Update static lists. Add various TLDs to always extract outlinks from.

--- update-static.py
+++ update-static.py
@@ -1,4 +1,7 @@
+import os
 import re
+import traceback
+import typing
 
 import requests
 
@@ -6,19 +9,51 @@ IANA_TLDS = 'https://data.iana.org/TLD/tlds-alpha-by-domain.txt'
 USER_AGENTS = 'https://www.useragents.me/'
 
 
-def update_tlds():
+def write_file(filename: str, data: typing.Union[str, typing.List[str]]) -> int:
+    if type(data) is list:
+        data = '\n'.join(data) + '\n'
+    result = None
+    temp_filename = filename + '.bak'
+    if os.path.isfile(temp_filename):
+        if not os.path.isfile(filename):
+            os.rename(temp_filename, filename)
+        else:
+            os.remove(temp_filename)
+    if os.path.isfile(filename):
+        os.rename(filename, temp_filename)
+    try:
+        with open(filename, 'w') as f:
+            result = f.write(data)
+    except Exception:
+        traceback.print_exc()
+        if os.path.isfile(filename):
+            os.remove(filename)
+    finally:
+        if not os.path.isfile(filename) \
+            and os.path.isfile(temp_filename):
+            os.rename(temp_filename, filename)
+        if os.path.isfile(filename) \
+            and os.path.isfile(temp_filename):
+            os.remove(temp_filename)
+    return result
+
+
+def update_tlds() -> typing.List[str]:
+    print('Updating TLDs.')
     response = requests.get(IANA_TLDS)
     assert response.status_code == 200 and len(response.content) > 0
-    with open('static-tlds.txt', 'w') as f:
-        f.write('onion\n')
-        for line in response.text.splitlines():
-            line = line.strip()
-            if len(line) == 0 or line.startswith('#'):
-                continue
-            f.write(line.lower()+'\n')
+    tlds = ['onion']
+    for line in response.text.splitlines():
+        line = line.strip()
+        if len(line) == 0 or line.startswith('#'):
+            continue
+        tlds.append(line.lower())
+    write_file('static-tlds.txt', tlds)
+    return tlds
 
 
-def update_uas():
+def update_uas() -> typing.List[str]:
+    print('Updating user-agents.')
     response = requests.get(USER_AGENTS)
     assert response.status_code == 200 and len(response.content) > 0
     user_agents = {}
@@ -33,10 +68,31 @@ def update_uas():
     for browser, versions in user_agents.items():
         user_agents[browser] = sorted(versions.items(), key=lambda x: x[0], reverse=True)[0][1]
     if len(user_agents) > 0:
-        with open('user-agents.txt', 'w') as f:
-            f.write('\n'.join(sorted(user_agents.values())))
+        user_agents = sorted(user_agents.values())
+        write_file('user-agents.txt', user_agents)
+    else:
+        user_agents = []
+    return user_agents
+
+
+def update_outlinks_domains() -> typing.List[str]:
+    print('Updating outlinks list.')
+    lines = {'arpa', 'gov', 'mil', 'museum', 'edu'} # 'org'
+    tlds = update_tlds()
+    with open('static-extract-outlinks-domains.txt', 'r') as f:
+        for line in f:
+            line = line.strip()
+            if len(line) == 0:
+                continue
+            if '.' not in line:# and line not in tlds:
+                continue
+            lines.add(line)
+    lines = sorted(lines)
+    write_file('static-extract-outlinks-domains.txt', lines)
+    return lines
 
 if __name__ == '__main__':
     update_tlds()
     update_uas()
+    update_outlinks_domains()
 
