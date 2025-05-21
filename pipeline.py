13317034dcbec642bc9e111bac0cc8863ac52db0
arkiver <arkiver@protonmail.com>
2025-05-21 17:51:46 +0200

    Version 20250521.01. Increase total timeout to 100 seconds per URL.

--- pipeline.py
+++ pipeline.py
@@ -84,7 +84,7 @@ WGET_AT_COMMAND = [WGET_AT]
 #
 # Update this each time you make a non-cosmetic change.
 # It will be added to the WARC files and reported to the tracker.
-VERSION = '20250518.02'
+VERSION = '20250521.01'
 #USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.183 Safari/537.36'
 TRACKER_ID = 'urls'
 TRACKER_HOST = 'legacy-api.arpa.li'
@@ -393,7 +393,7 @@ class WgetArgs(object):
             item['custom_items'] = '{}'
             return realize(['sleep', '0'], item)
 
-        command = ['timeout', str(int((item['item_name'].count('\0')+1)*40))] + WGET_AT_COMMAND
+        command = ['timeout', str(int((item['item_name'].count('\0')+1)*100))] + WGET_AT_COMMAND
         print('Using global timeout', command[1])
 
         wget_args = command + [
