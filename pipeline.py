d2e522af240b9cff2bcddf8d73a74c4a00df22f2
s-crypt <41712656+s-crypt@users.noreply.github.com>
2025-07-31 23:06:06 -0700

    use https for all AT services (#16)
    * use https for all AT services
    
    https was used inconsistently for URLs
    
    * update version
    
    ---------
    
    Co-authored-by: Katie Holly <git@meo.ws>

--- pipeline.py
+++ pipeline.py
@@ -84,7 +84,7 @@ WGET_AT_COMMAND = [WGET_AT]
 #
 # Update this each time you make a non-cosmetic change.
 # It will be added to the WARC files and reported to the tracker.
-VERSION = '20250718.01'
+VERSION = '20250801.01'
 #USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.183 Safari/537.36'
 TRACKER_ID = 'urls'
 TRACKER_HOST = 'legacy-api.arpa.li'
@@ -125,7 +125,7 @@ class CheckIP(SimpleTask):
                 'capture_output': True
             }
 
-            url = 'http://legacy-api.arpa.li/now'
+            url = 'https://legacy-api.arpa.li/now'
             returned = subprocess.run(
                 command+[url],
                 **kwargs
@@ -482,7 +482,7 @@ project = Project(
     title = 'URLs',
     project_html = '''
     <img class="project-logo" alt="logo" src="https://archiveteam.org/images/thumb/f/f3/Archive_team.png/235px-Archive_team.png" height="50px"/>
-    <h2>Archiving sets of discovered outlinks. &middot; <a href="http://tracker.archiveteam.org/urls/">Leaderboard</a></span></h2>
+    <h2>Archiving sets of discovered outlinks. &middot; <a href="https://tracker.archiveteam.org/urls/">Leaderboard</a></span></h2>
     '''
 )
 
