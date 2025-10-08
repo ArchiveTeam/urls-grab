6f756d54e04f7b16bdd347a3c4e2db2d7da14b65
arkiver <arkiver@protonmail.com>
2025-10-08 18:38:24 +0200

    Make apt noninteractive.

--- warrior-install.sh
+++ warrior-install.sh
@@ -3,8 +3,8 @@
 if ! dpkg-query -Wf'${Status}' ghostscript 2>/dev/null | grep -q '^i'
 then
   echo "Installing ghostscript..."
-  sudo apt-get update
-  sudo apt-get install -y --no-install-recommends ghostscript || exit 1
+  sudo DEBIAN_FRONTEND=noninteractive DEBIAN_PRIORITY=critical apt-get -qqy --no-install-recommends -o Dpkg::Options::=--force-confdef -o Dpkg::Options::=--force-confold -o Dpkg::Options::=--force-unsafe-io update
+  sudo DEBIAN_FRONTEND=noninteractive DEBIAN_PRIORITY=critical apt-get -qqy --no-install-recommends -o Dpkg::Options::=--force-confdef -o Dpkg::Options::=--force-confold -o Dpkg::Options::=--force-unsafe-io install ghostscript || exit 1
   sudo rm -rf /var/lib/apt/lists/*
 fi
 
