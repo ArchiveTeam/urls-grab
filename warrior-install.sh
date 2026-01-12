8623ecf4b68cb5aed80305cf1f36dbcb386eee8d
arkiver <arkiver@protonmail.com>
2026-01-12 15:52:23 +0100

    Version 20260112.02. Simplify install of pip and apt packages.

--- warrior-install.sh
+++ warrior-install.sh
@@ -3,35 +3,19 @@
 if ! dpkg-query -Wf'${Status}' ghostscript 2>/dev/null | grep -q '^i'
 then
   echo "Installing ghostscript..."
-  sudo -n DEBIAN_FRONTEND=noninteractive DEBIAN_PRIORITY=critical \
-    apt-get -qqy --no-install-recommends \
-    -o Dpkg::Options::=--force-confdef \
-    -o Dpkg::Options::=--force-confold \
-    -o Dpkg::Use-Pty=0 \
-    update
-  sudo -n DEBIAN_FRONTEND=noninteractive DEBIAN_PRIORITY=critical \
-    apt-get -qqy --no-install-recommends \
-    -o Dpkg::Options::=--force-confdef \
-    -o Dpkg::Options::=--force-confold \
-    -o Dpkg::Status-Fd=1 \
-    -o Dpkg::Use-Pty=0 \
-    install ghostscript \
-    2>&1 || exit 1
-  sudo -n apt-get clean
+  sudo apt-get update
+  sudo apt-get install -y --no-install-recommends ghostscript || exit 1
   sudo rm -rf /var/lib/apt/lists/*
 fi
 
-if ! python3 -c "import validators" 2>/dev/null
-then
-  echo "Installing Python3 validators..."
-  sudo python3 -m pip install --no-cache-dir validators || exit 1
-fi
-
-if ! python3 -c "import lupa" 2>/dev/null
-then
-  echo "Installing Python3 lupa..."
-  sudo python3 -m pip install --no-cache-dir lupa || exit 1
-fi
+for package in validators lupa
+do
+  if ! python3 -c "import ${package}" 2>/dev/null
+  then
+    echo "Installing Python3 ${package}..."
+    sudo python3 -m pip install --no-cache-dir "${package}" || exit 1
+  fi
+done
 
 exit 0
 
