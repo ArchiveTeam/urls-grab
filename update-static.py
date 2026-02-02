e36ac72caabe6465ddd92b89bed3f365615a730f
arkiver <arkiver@protonmail.com>
2026-02-02 12:46:55 +0100

    Version 20260202.01. Add edu/gov/etc. public suffixes to extract outlinks to and from.

--- update-static.py
+++ update-static.py
@@ -7,6 +7,107 @@ import requests
 
 IANA_TLDS = 'https://data.iana.org/TLD/tlds-alpha-by-domain.txt'
 USER_AGENTS = 'https://www.whatismybrowser.com/guides/the-latest-user-agent/firefox'
+PUBLIC_SUFFIX_LIST = 'https://raw.githubusercontent.com/publicsuffix/list/master/public_suffix_list.dat'
+SPECIAL_TLDS = {'arpa', 'gov', 'mil', 'museum', 'edu', 'org'}
+SPECIAL_LABELS = (
+    {
+        "academy",
+        "education",
+        "edu",
+        "ed",
+        "institute",
+        "sch",
+        "school",
+        "schools",
+        "k12",
+        "college",
+        "university",
+        "uni",
+        "univ",
+        "student",
+        "schule",
+    }
+    | {
+        "gov",
+        "gob",
+        "go",
+        "gouv",
+        "govt",
+        "gobierno",
+        "gouvernement",
+        "government",
+        "mil",
+        "army",
+        "navy",
+        "airforce",
+        "lg",
+        "gub",
+        "state",
+        "county",
+        "commune",
+        "muni",
+        "municipal",
+        "municipio",
+        "council",
+        "parliament",
+        "pref",
+        "prefecture",
+        "province",
+        "region",
+        "department",
+        "district",
+        "adm",
+        "dep",
+        "kommune",
+    }
+    | {
+        "org",
+        "or",
+        "ngo",
+        "ong",
+        "charity",
+        "foundation",
+        "giving",
+        "gives",
+        "aid",
+        "asso",
+        "assoc",
+        "association",
+        "assn",
+    }
+    | {
+        "int",
+        "intl",
+        "international"
+    }
+    | {
+        "health",
+        "healthcare",
+        "hospital",
+        "hosp",
+        "clinic",
+        "clinique",
+        "care",
+        "doctor",
+        "dentist",
+        "pharmacy",
+        "med",
+        "medecin",
+        "medicina",
+        "vet",
+        "salud",
+        "nhs",
+        "police",
+        "fire",
+        "ambulance",
+        "rescue",
+        "emergency",
+        "safety",
+        "security",
+        "mod",
+    }
+)
+SPECIAL_LABELS_NOT_ENDING = {"ac", "city", "gop"}
 
 
 def write_file(filename: str, data: typing.Union[str, typing.List[str]]) -> int:
@@ -72,16 +173,30 @@ def update_uas() -> typing.List[str]:
 
 
 def update_outlinks_domains() -> typing.List[str]:
-    lines = {'arpa', 'gov', 'mil', 'museum', 'edu', 'org'}
-    tlds = update_tlds()
+    lines = set()
+    lines |= SPECIAL_TLDS
     with open('static-extract-outlinks-domains.txt', 'r') as f:
         for line in f:
             line = line.strip()
             if len(line) == 0:
                 continue
-            if '.' not in line:# and line not in tlds:
+            if '.' not in line:
                 continue
             lines.add(line.lower())
+    response = requests.get(PUBLIC_SUFFIX_LIST)
+    assert response.status_code == 200 and len(response.text) > 100000
+    for line in response.text.splitlines():
+        line = line.strip().lower()
+        if len(line) == 0 or line.startswith('//'):
+            continue
+        line = line.strip('*.!').split('.')
+        if any(label in SPECIAL_LABELS for label in line) \
+            or any(label in SPECIAL_LABELS_NOT_ENDING for label in line[:-1]):
+            lines.add('.'.join(line))
+            continue
+    for tld in update_tlds():
+        if tld in (SPECIAL_LABELS | SPECIAL_TLDS) and tld not in SPECIAL_LABELS_NOT_ENDING:
+            lines.add(tld)
     lines = sorted(lines)
     write_file('static-extract-outlinks-domains.txt', lines)
     return lines
