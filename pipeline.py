28ccf35d80668c996bdf0bad04d4b6e2bd03b169
arkiver <arkiver@protonmail.com>
2026-09-25 19:05:15 +0200

    Version 20260925.01. Set timeout to 10 seconds.

--- pipeline.py
+++ pipeline.py
@@ -88,7 +88,7 @@ WGET_AT_COMMAND = [WGET_AT]
 #
 # Update this each time you make a non-cosmetic change.
 # It will be added to the WARC files and reported to the tracker.
-VERSION = '20260924.01'
+VERSION = '20260925.01'
 #USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.183 Safari/537.36'
 TRACKER_ID = 'urls'
 TRACKER_HOST = 'legacy-api.arpa.li'
@@ -465,7 +465,7 @@ class WgetArgs(object):
             '-e', 'robots=off',
             '--recursive', '--level=inf',
             '--no-parent',
-            '--timeout', '20',
+            '--timeout', '10',
             '--tries', '2',
             '--span-hosts',
             '--page-requisites',
